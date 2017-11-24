43224a242c8b367ff3432deaf8736c9c551f63ad
thanhlong <thanhlong09041995@gmail.com>
2017-11-24 14:10:20 +0700

    fix

--- php_progamer_november/week_3/18_string_integer_boolean.php
+++ php_progamer_november/week_3/18_string_integer_boolean.php
@@ -10,25 +10,20 @@ for ($id =1; $id<10;$id++)
 $product =  "Tổng quát về gian hàng Vstore";
 $shopping_cart1 = "Website bất động sản";
 $shopping_cart2 = "Website đồ uống, ẩm thực & thời trang";
-$shopping_cart3 = "Website thiết bị điện tử di động";
 $detail_cart1 = "Dự án Sunshine City".'<br>'."Resort Ha Long bay";
 $detail_cart2 = "Coffe Highland".'<br>'."Royal Restaurant"."
 Fashion Chanel";
-$detail_cart3 = "Samsung TV".'<br>'."Iphone 8 & Iphone X";
 $count_Detail_cart1 = 10;
 $count_Detail_cart2 = 50;
-$count_Detail_cart3 = 30;
-$productAll = $product.'<br>'.$shopping_cart1.'<br>'.$shopping_cart2.'<br>'.$shopping_cart3;
+$productAll = $product.'<br>'.$shopping_cart1.'<br>'.$shopping_cart2.'<br>';
 $array_shopping_cart1 = $shopping_cart1.'<br>'.$detail_cart1.'<br>'.$count_Detail_cart1;
 $array_shopping_cart2 = $shopping_cart2.'<br>'.$detail_cart2.'<br>'.$count_Detail_cart2;
-$array_shopping_cart3 = $shopping_cart3.'<br>'.$detail_cart3.'<br>'.$count_Detail_cart3;
 
 echo '<h1>In chuỗi string</h1>';
 // in ra chuỗi string
 echo "<ul>$product";
 echo "<li>$shopping_cart1</li>";
 echo "<li>$shopping_cart2</li>";
-echo "<li>$shopping_cart3</li>";
 echo "</ul>";
 echo '<h1>In hoa, in thường chuỗi string</h1>';
 
@@ -58,12 +53,17 @@ print_r('Có'.' '.(strlen($product)).' '.'Kí tự');
 
 
 echo '<h1>Chuyển đổi sang mảng array</h1>';
+/** tách và gộp hai mảng $productSmartphone và $stock
+ * @param string $productSmartphone & $stock
+ * @return array|null
+ */
 $productSmartphone = "Iphone, Samsung, Nokia, Xiaomi";
 $stock = "22, 33, 45, 12";
 echo '<pre>';
 $arrayexplode_smartphone = explode(',',$productSmartphone);
 $arrayexplode_stock = explode(',',$stock);
 
+// Trộn 2 mảng có $key là $arrayexplode_smartphone với giá trị $value là $arrayexplode_stock
 $arraycombine_smartphone = array_combine($arrayexplode_smartphone,$arrayexplode_stock);
 print_r($arraycombine_smartphone);
 ?>
@@ -99,14 +99,6 @@ print_r($arraycombine_smartphone);
             <td><?=$shopping_cart2?></td>
             <td><?=$detail_cart2?></td>
             <td><?=$count_Detail_cart2?></td>
-            <td><a href="javascript:;">Sửa</a></td>
-            <td><a href="javascript:;">Xóa</a></td>
-        </tr>
-        <tr>
-            <td><?=$id?></td>
-            <td><?=$shopping_cart3?></td>
-            <td><?=$detail_cart3?></td>
-            <td><?=$count_Detail_cart3?></td>
             <td><a href="javascript:;">Sửa</a></td>
             <td><a href="javascript:;">Xóa</a></td>
         </tr>
