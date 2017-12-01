d19f81ed7eac1ae2da69df2204d20e8679d9a77b
thanhlong <thanhlong09041995@gmail.com>
2017-12-01 16:12:34 +0700

    fix

---
+++ shoping_cart/inc-dbcontroller.php
@@ -0,0 +1,148 @@
+<?php
+if(!isset($_SESSION))
+{
+    session_start();
+}
+require_once'product.php';
+class DBController {
+    /** Kiểm tra xem session có tồn tại hay không
+     * @param $key
+     * @return bool
+     */
+
+    public static function exits($key)
+    {
+        return (isset($_SESSION[$key])) ? true : false;
+    }
+
+    /**  Kiểm tra xem session có trống hay không
+     * @param $key
+     * @return bool|null
+     */
+    public static function null($key)
+    {
+        return ($_SESSION[$key] != null) ? null : false;
+    }
+
+    /** Gán mảng có key với giá trị value
+     * @param $key
+     * @param $value
+     * @return mixed
+     */
+
+    public static function put($key,$value)
+    {
+        return $_SESSION[$key] = $value;
+    }
+
+    /** lấy giá trị của mảng
+     * @param $key
+     * @return null
+     */
+    public static function get($key)
+    {
+        return self::exits($key) ? $_SESSION[$key] : null;
+    }
+
+    /** Xóa giá trị của mảng
+     * @param $key
+     * @return int|void
+     */
+
+    public static function delete($key)
+    {
+        return self::exits($key) <= 0 ? session_unset($_SESSION[$key]) : $_SESSION[$key] -= 1;
+    }
+}
+class Product
+{
+    public function getProduct()
+    {
+        $product =
+            [
+              'id' => $this->id,
+              'name' => $this->name,
+              'img' => $this->img,
+              'manufacturer' => $this->manufacturer,
+              'stock' => $this->stock,
+            ];
+        return $product;
+    }
+}
+class Cart extends Product
+{
+
+    public $newProduct = array();
+    // Thêm giá trị vào mảng
+    public function insertCart()
+    {
+
+        $idProduct = $_GET['id'];
+        $newProduct = array();
+        // them vao mang
+
+        foreach ($product as $key => $value) {
+            $newProduct[$value['id']] = $value;
+        }
+
+        // Kiem tra xem trong mang do da co gio hang chua hoac gio hang do trong
+        if (!isset($_SESSION['giohang']) || $_SESSION['giohang'] ==null) {
+            $newProduct[$idProduct]['qty'] = 1;
+            $_SESSION['giohang'][$idProduct] = $newProduct[$idProduct];
+        }else{
+            // da ton tai gio hang va san pham do da co thi tang len 1
+            if (array_key_exists($idProduct,$_SESSION['giohang'])){
+                $_SESSION['giohang'][$idProduct]['qty'] +=1;
+            }else{
+                $newProduct[$idProduct]['qty'] = 1;
+                $_SESSION['giohang'][$idProduct] = $newProduct[$idProduct];
+            }
+        }
+        header("location: index.php");
+    }
+    // Xóa Giá trị của mảng
+    public function deleteCart()
+    {
+        $id =$_GET['id'];
+        // kiem tra xem o so luong trong gio hang co gia tri am khong.
+        if ($_SESSION['giohang'][$id]['qty'] <=0) {
+            unset($_SESSION['giohang'][$id]);
+        }else{
+            $_SESSION['giohang'][$id]['qty'] -=1;
+
+        }
+        header("location:interface.php");
+    }
+    // Cập nhập giá trị của mảng
+    public function updateCart()
+    {
+        /**
+         *
+         */
+        if (isset($_POST['btnUpdate'])) {
+            foreach ($_POST['qty'] as $key => $value) {
+                if ($_POST['qty'] <0) {
+                    unset($_SESSION['giohang'][$key]);
+                }else{
+                    // cap nhat gia tri cua so luong trong gio hang
+                    $_SESSION['giohang'][$key]['qty'] = $value;
+                }
+            }
+            header("location:index.php");
+        }
+    }
+    // Đếm số lượng giá trị của mảng
+    public static function totalCart()
+    {
+        $total = 0;
+        /** kiem tra xem ton tai gio hang khong va khac trong không
+         *
+         */
+        if (isset($_SESSION['giohang']) && $_SESSION['giohang'] != null) {
+            foreach ($_SESSION['giohang'] as $val) {
+                $total += $val['qty'];
+            }
+        }
+    }
+}
+?>
---
+++ shoping_cart/index.php
@@ -0,0 +1,53 @@
+<html>
+<?php
+if(!isset($_SESSION))
+{
+    session_start();
+}
+require_once("product.php");
+require_once ("inc-dbcontroller.php");
+$db_handle = new DBController();
+?>
+<head>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+    </head>
+</head>
+<body>
+<form action="" method="post" role="form">
+    <legend ><h1 style="text-align: center">Bạn Đang Có Tống <a href="inc-view-cart.php"><?php echo $value['total']?></a> Sản phẩm Trong Giỏ Hàng</h1></legend>
+    <?php foreach ($product as $key => $value) { ?>
+
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Tên sản phẩm</th>
+                <th>Hình ảnh minh họa</th>
+                <th>Hãng sản xuất</th>
+                <th>Số lượng</th>
+                <th>Đơn giá</th>
+                <th>Mua hàng</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td><?php echo $value['id'] ?></td>
+                <td><?php echo $value['name'] ?></td>
+                <td><img class="img-reponsive" style="max-width: 50px" src="<?php echo $value['img'] ?>" alt=""></td>
+                <td><?php echo $value['manufacturer'] ?></td>
+                <td><?php echo $value['stock'] ?></td>
+                <td><?php echo $value['price'] ?></td>
+                <td><input type="submit" value="Mua Ngay" name="mua ngay"></td>
+            </tr>
+            </tbody>
+        </table>
+        <?php } ?>
+</form>
+</body>
+</html>
