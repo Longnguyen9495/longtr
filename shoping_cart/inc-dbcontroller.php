ca3a01e85e20a58fc4983566bc2282797f6b7cda
thanhlong <thanhlong09041995@gmail.com>
2017-12-01 17:20:25 +0700

    fix

--- shoping_cart/inc-dbcontroller.php
+++ shoping_cart/inc-dbcontroller.php
@@ -1,10 +1,71 @@
 <?php
-if(!isset($_SESSION))
+if (!isset($_SESSION))
 {
     session_start();
 }
-require_once'product.php';
-class DBController {
+include('product.php');
+
+echo '<pre>';
+
+
+
+class Product
+{
+    public $product = [
+        array[
+            'id' => 1,
+            'name' => 'Galaxy s8',
+            'img' => 'http://images.samsung.com/is/image/samsung/p5/vn/smartphones/galaxy-s8/images/gallery/galaxy-s8-plus_gallery_left_side_black_s4.png?$ORIGIN_PNG$',
+            'price' => 1000,
+            'manufacturer' => 'Sam sung',
+            'stock' => '20'
+        ],
+        array[
+            'id' => 2,
+            'name' => 'Iphone X',
+            'img' => 'https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone/x/iphone-x-select-2017?wid=189&hei=376&fmt=png-alpha&qlt=95&.v=1504378258086',
+            'price' => 2000,
+            'manufacturer' => 'Apple',
+            'stock' => '14'
+        ],
+            array[
+            'id' => 3,
+            'name' => 'Nokia 8',
+            'img' => 'https://images.contentful.com/wcfotm6rrl7u/5TrLOIBnNeeUWU68kMuaoC/9e0a5bc67301c48fbb88b64208e3f0f4/Nokia_8-hero_still_smaller_Optimised-3.png?fm=png&q=80',
+            'price' => 4000,
+            'manufacturer' => 'Nokia',
+            'stock' => '26'
+        ],
+                array[
+            'id' => 4,
+            'name' => 'Mi mix 2',
+            'img' => 'https://bachlongmobile.com/media/clnews/1508131037170332201.jpg',
+            'price' => 3000,
+            'manufacturer' => 'Xiaomi',
+            'stock' => '12'
+        ],
+    ];
+    public function getProduct()
+    {
+
+        $products =
+            [
+                'id' => $this->id,
+                'name' => $this->name,
+                'img' => $this->img,
+                'price'=> $this->price,
+                'manufacturer' => $this->manufacturer,
+                'stock' => $this->id
+            ];
+        return $products;
+    }
+}
+$Pd = new Product();
+$Pd->getProduct();
+print_r($Pd->getProduct());
+
+class DBController
+{
     /** Kiểm tra xem session có tồn tại hay không
      * @param $key
      * @return bool
@@ -30,7 +91,7 @@ class DBController {
      * @return mixed
      */
 
-    public static function put($key,$value)
+    public static function put($key, $value)
     {
         return $_SESSION[$key] = $value;
     }
@@ -49,30 +110,17 @@ class DBController {
      * @return int|void
      */
 
-    public static function delete($key)
-    {
-        return self::exits($key) <= 0 ? session_unset($_SESSION[$key]) : $_SESSION[$key] -= 1;
-    }
+//    public static function delete($key)
+//    {
+//        return self::exits($key) <= 0 ? session_unset($_SESSION[$key]) : $_SESSION[$key] -= 1;
+//    }
 }
-class Product
-{
-    public function getProduct()
-    {
-        $product =
-            [
-              'id' => $this->id,
-              'name' => $this->name,
-              'img' => $this->img,
-              'manufacturer' => $this->manufacturer,
-              'stock' => $this->stock,
-            ];
-        return $product;
-    }
-}
+
 class Cart extends Product
 {
 
     public $newProduct = array();
+
     // Thêm giá trị vào mảng
     public function insertCart()
     {
@@ -81,49 +129,51 @@ class Cart extends Product
         $newProduct = array();
         // them vao mang
 
-        foreach ($product as $key => $value) {
-            $newProduct[$value['id']] = $value;
-        }
+//        foreach ($product as $key => $value) {
+//            $newProduct[$value['id']] = $value;
+//        }
 
         // Kiem tra xem trong mang do da co gio hang chua hoac gio hang do trong
-        if (!isset($_SESSION['giohang']) || $_SESSION['giohang'] ==null) {
+        if (!isset($_SESSION['giohang']) || $_SESSION['giohang'] == null) {
             $newProduct[$idProduct]['qty'] = 1;
             $_SESSION['giohang'][$idProduct] = $newProduct[$idProduct];
-        }else{
+        } else {
             // da ton tai gio hang va san pham do da co thi tang len 1
-            if (array_key_exists($idProduct,$_SESSION['giohang'])){
-                $_SESSION['giohang'][$idProduct]['qty'] +=1;
-            }else{
+            if (array_key_exists($idProduct, $_SESSION['giohang'])) {
+                $_SESSION['giohang'][$idProduct]['qty'] += 1;
+            } else {
                 $newProduct[$idProduct]['qty'] = 1;
                 $_SESSION['giohang'][$idProduct] = $newProduct[$idProduct];
             }
         }
         header("location: index.php");
     }
+
     // Xóa Giá trị của mảng
     public function deleteCart()
     {
-        $id =$_GET['id'];
+        $id = $_GET['id'];
         // kiem tra xem o so luong trong gio hang co gia tri am khong.
-        if ($_SESSION['giohang'][$id]['qty'] <=0) {
+        if ($_SESSION['giohang'][$id]['qty'] <= 0) {
             unset($_SESSION['giohang'][$id]);
-        }else{
-            $_SESSION['giohang'][$id]['qty'] -=1;
+        } else {
+            $_SESSION['giohang'][$id]['qty'] -= 1;
 
         }
         header("location:interface.php");
     }
+
     // Cập nhập giá trị của mảng
     public function updateCart()
     {
-        /**
+        /** cập nhập giá trị của mảng xem có tồn tại hay không
          *
          */
         if (isset($_POST['btnUpdate'])) {
             foreach ($_POST['qty'] as $key => $value) {
-                if ($_POST['qty'] <0) {
+                if ($_POST['qty'] < 0) {
                     unset($_SESSION['giohang'][$key]);
-                }else{
+                } else {
                     // cap nhat gia tri cua so luong trong gio hang
                     $_SESSION['giohang'][$key]['qty'] = $value;
                 }
@@ -131,6 +181,7 @@ class Cart extends Product
             header("location:index.php");
         }
     }
+
     // Đếm số lượng giá trị của mảng
     public static function totalCart()
     {
@@ -145,4 +196,5 @@ class Cart extends Product
         }
     }
 }
+
 ?>
