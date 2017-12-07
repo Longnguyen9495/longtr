c9036c87a0e0f7c41718f1550721fd22d3d588a8
thanhlong <thanhlong09041995@gmail.com>
2017-12-07 17:32:56 +0700

    add file

--- php_progamer_december/week1/info/edit.php
+++ php_progamer_december/week1/info/edit.php
@@ -1,1 +1,20 @@
 <?php
+    session_start();
+echo '<pre>';
+    print_r($_SESSION['start']);
+    header('location:')
+ ?>
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+</head>
+<body>
+<form action="" style="margin: 10px">
+    Sửa danh sách :
+    <br></br>
+    <input type="text" value="<?php echo $_SESSION['start']['name']?>" placeholder="name..."><br></br>
+    <input type="text" value="<?php echo $_SESSION['start']['year']?>" name="year" placeholder="year..."><br></br>
+    <input type="submit" name="submit" value="Sửa"><br></br>
+</form>
+</body>
+</html>
